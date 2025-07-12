ed89b65f3e5a96b22d2f7d00fb47682875288a3f
Amit Yadav <amit931552@gmail.com>
2025-07-12 20:54:14 +0530

    etnlket

---
+++ src/Pattern_/Pattern1.java
@@ -0,0 +1,24 @@
+package Pattern_;
+
+public class Pattern1 {
+
+    static void pattern1( int n){
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;  j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+          Pattern1  obj1=new Pattern1();
+          obj1.pattern1(5);
+
+    }
+}
